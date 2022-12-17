2c0b684174aa825981f80f909291a38e0f9941c8
Willian <willian.m.sousa@gmail.com>
2022-12-17 11:54:53 -0300

    Rotas criadas, bootstap e axios instalados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Quebra!</h1>
+      <Routes>
+        <Route path='/'element={ <HomePage />} />
+      </Routes>
+      
     </div>
   );
 }
