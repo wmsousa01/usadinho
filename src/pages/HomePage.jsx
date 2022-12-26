aa797aab1f93df3e70b5d563d0dbdc2e5f73ba29
Bruno Malachiasgit config --global user.email malachiasbruno@gmail.comgit config --global user.name Bruno <malachiasbruno@gmail.com>
2022-12-26 17:28:32 -0300

    LayOut Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
+import '../pages/HomePage.css'
+import background from '../images/BGHomePage.png'
 
 
 
 const HomePage = () => {
   return (
-    <div className="HomePage">
-      <div className='row'>
-        <div className='col'>
-          <h1>Usadinho</h1>
-        </div>
+    <div className="HomePage" style={{backgroundImage:`url(${background})`}}>
+      <div className='header'>
+        <h1 className='h1'>U</h1>
+        <div className='qv'>Quero Vender</div>
+
       </div>
-      <div className='row'>
-        <div className='col'>
-        Palavra 1
+        
+      <div className="row">
+        <div className="col">
+        <div className="card" style={{width: '20vw'}}>
+            <div className="card-body">
+              <h5 className="card-title">Querendo divulgar aquelas bugigangas que não usa mais?</h5>
+                <p className='card-text'>NOME</p>
+                <input></input>
+                <p className='card-text'>TELEFONE</p>
+                <input></input>
+                <p className='card-text'>EMAIL</p>
+                <input></input>
+                <div className='divCardInicial'>
+                  <a href="#" className='btn btn-primary'>Enviar</a>
+                </div>
+                
+            </div>
         </div>
-      <div className='col'>
-        Palavra 2
+        </div>
+
       </div>
-    </div>
     </div>
     
   )
