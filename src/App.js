eb0e8f1d3bc1905482e4f5e0005617f266d6015d
Willian <willian.m.sousa@gmail.com>
2023-01-05 22:16:32 -0300

    ProductsPage , Link Api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import ViewCollectionPage from './pages/ViewCollectionPage';
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path='/'element={ <HomePage />} />
-        <Route path='/ProductPage'element={ <ProductPage />} />
+        <Route path='/ProductPage/:itemId' element={ <ProductPage />} />
+        <Route path='/view-collection' element={ <ViewCollectionPage /> } />
       </Routes>
       
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Bike from '../images/bike.png'
 
-const Card = () => {
+const Card = ({ product}) => {
   return (
-    <div className='p-3'>
+    <div>
         <div className="card" style={{width: "18rem"}}>
-        <img src={Bike} className="card-img-top" alt="..." />
+        <img src={product.imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
